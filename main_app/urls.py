ae33688e97799b5081bc017997680baaf8e7e448
Grace Clower <clower.grace@gmail.com>
2024-08-21 16:29:37 -0400

    set up recipe serializer, created two main routes getting all recipes and retrieving, updating, and deleting a recipe by id

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import Home, RecipeList, RecipeDetail
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
 
+    path('recipes/', RecipeList.as_view(), name='recipe-list'),
+    path('recipes/<int:id>/', RecipeDetail.as_view(), name='recipe-detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
+from .models import Recipe
+from .serializers import RecipeSerializer
 
 class Home(APIView):
     def get(self,request):
         content = {'message': 'Welcome to the recipes api home route!'}
         return Response(content)
+    
+class RecipeList(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    lookup_field = 'id'
