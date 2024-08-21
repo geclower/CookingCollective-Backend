4eb5f584347e0c325b65b10ce5ce20601102af84
Grace Clower <clower.grace@gmail.com>
2024-08-21 16:01:14 -0400

    sketched out model ERD, created model, migrated to db

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+CATEGORIES = (
+    ('beef', 'Beef'),
+    ('chic', 'Chicken'),
+    ('dess', 'Dessert'),
+    ('lamb', 'Lamb'),
+    ('misc', 'Miscellaneous'),
+    ('past', 'Pasta'),
+    ('pork', 'Pork'),
+    ('seaf', 'Seafood'),
+    ('side', 'Side'),
+    ('star', 'Starter'),
+    ('vega', 'Vegan'),
+    ('vege', 'Vegetarian'),
+    ('brea', 'Breakfast'),
+    ('goat', 'Goat'),
+)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=4, choices=CATEGORIES, default=CATEGORIES[0][0])
+    instructions = models.TextField()
+    image = models.CharField()
+    video = models.CharField()
+    ingredients = ArrayField(models.CharField(), blank=True, null=True)
+    source = models.CharField()
+
+    def __str__(self):
+        return self.name
